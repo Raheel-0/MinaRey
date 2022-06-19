2106db30e600a3a42b1b6de55b0ed4746e929362
Raheel-0 <107804611+Raheel-0@users.noreply.github.com>
2022-06-19 16:20:50 +0100

    upload files :)

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let h = 0, v = 0
+let tex;
+let song;
+let font;
+
+
+function preload() { 
+	song = loadSound('Nandemonaiya & Sparkle - Kimi no Na wa. (Acoustic Guitar).m4a'); 
+
+}
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL);
+	tex = loadImage('yasminka.jpg');
+	song.play();
+
+}
+
+function draw() {
+	smooth();
+	background(0);
+	noStroke();
+	camera(0, 0, 0, 0, 0, 1, 0, 1, 0);
+	scale(-1, 1, 1)
+	rotateX(radians(v));
+	rotateY(radians(h));
+
+	texture(tex);
+	sphere(1000);
+
+	if (keyIsDown(39)) h--;
+	if (keyIsDown(37)) h++;
+	if (keyIsDown(38) && v > -90) v--;
+	if (keyIsDown(40) && v < 90) v++;
+
+	if (mouseIsPressed) {
+		h -= (mouseX - width / 2) / width;
+		v = min(90, max(-90, v + (mouseY - height / 2) / height));
+	}
+
+}
+
